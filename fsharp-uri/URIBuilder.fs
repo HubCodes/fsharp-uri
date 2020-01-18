cfdec7bae89ec0444f73f1c4b5785e3f69008234
Taemin Cha <krlrhkstk12@gmail.com>
2020-01-18 18:37:03 +0900

    Add basic operations

--- fsharp-uri/URIBuilder.fs
+++ fsharp-uri/URIBuilder.fs
@@ -1,5 +1,41 @@
 ﻿namespace FsharpURI
 
+open System
+
+type Scheme =
+  | HTTP
+  | HTTPS
+  | FTP
+  | SFTP
+  | ABOUT
+  | DNS
+  | MAILTO
+  | Custom of string
+with
+  override this.ToString () =
+    match this with
+    | HTTP -> "http"
+    | HTTPS -> "https"
+    | FTP -> "ftp"
+    | SFTP -> "sftp"
+    | ABOUT -> "about"
+    | DNS -> "dns"
+    | MAILTO -> "mailto"
+    | Custom s -> s
+
+type URL = Scheme * string
+
+
 module URIBuilder =
-  let hello name =
-    printfn "Hello %s" name
+
+  let concat ((scheme, left): URL) (right: string) =
+    URL (scheme, left + "/" + right)
+
+  let (/) = concat
+
+  let setPort (scheme, body) (port: uint16) =
+    URL (scheme, body + ":" + Convert.ToString port)
+
+  let (/@) = setPort
+
+  let url ((scheme, body): URL) = scheme.ToString() + "://" + body.ToString()
